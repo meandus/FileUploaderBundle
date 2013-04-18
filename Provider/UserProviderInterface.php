f4a6545724d6db2e87058718a74ff4626bbe3273
qooplmao <fitz@michaelfitzpatrick.co.uk>
2013-04-18 09:20:37 +0100

    Added methods from UserProvider

--- Provider/UserProviderInterface.php
+++ Provider/UserProviderInterface.php
@@ -2,7 +2,23 @@
 
 namespace QoopLmao\FileUploaderBundle\Provider;
 
+use QoopLmao\FileUploaderBundle\Model\UserInterface;
+
 interface UserProviderInterface
 {
+    /**
+     * Gets the current authenticated user
+     *
+     * @return UserInterface
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     */
+    public function getAuthenticatedUser();
 
+    /**
+     * Checks for roles granted to current user
+     *
+     * @param $role
+     * @return bool
+     */
+    public function isGranted($role);
 }
